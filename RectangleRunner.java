316e447352d2424335782e2c437cdfcb71c079d5
Siddhi Amilkanthwar <66294249+siddhi9108@users.noreply.github.com>
2021-09-18 07:34:38 -0600

    Add files via upload

---
+++ RectangleRunner.java
@@ -0,0 +1,12 @@
+package com.siddhi.oops.level2;
+
+public class RectangleRunner {
+
+	public static void main(String[] args) {
+		Rectangle ob = new Rectangle(12, 32);
+		System.out.println(ob);
+		ob.setW(23);
+		System.out.println(ob);
+	}
+
+}
